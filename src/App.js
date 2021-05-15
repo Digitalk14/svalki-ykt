270b2e2048c753c8bb4aa5fb926d15814303a857
digitalk14 <dmitrii.vasiliev@indriver.com>
2021-05-15 09:15:46 +0900

    add admin

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import { ThemeProvider } from 'styled-components'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import { Main } from './components/main/main'
-import { Admin } from './components/admin/admin'
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Main } from "./components/main/main";
+import { Admin } from "./components/admin/admin";
+import { GetAdmins } from "./components/Login/getAdmins";
 
 export default class App extends React.Component {
   render() {
     return (
       <Router>
-        <ThemeProvider theme={{ fontFamily: 'Helvetica Neue' }}>
+        <ThemeProvider theme={{ fontFamily: "Helvetica Neue" }}>
           <Switch>
-            <Route exact path='/'>
+            <Route exact path="/">
               <Main />
             </Route>
-            <Route exact path='/admin'>
+            <Route exact path="/admin">
               <Admin />
+            </Route>
+            <Route exact path="/ykfj5768">
+              <GetAdmins />
             </Route>
           </Switch>
         </ThemeProvider>
@@ -22,4 +26,3 @@ export default class App extends React.Component {
     );
   }
 }
-
---
+++ src/components/Login/getAdmins.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import { GoogleLogin } from "react-google-login";
+import axios from 'axios'
+
+const clientId = process.env.GOOGLE_USER_ID;
+
+const LoginWrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+
+export const GetAdmins = () => {
+  const onSuccess = (res) => {
+      console.log(res.ft.Te)
+    axios({
+      url: "/api/addAdmin.php",
+      method: "post",
+      data: {
+        name: res.ft.Te,
+        googleId: res.googleId,
+      },
+    }).then((res) => {
+        console.log(res)
+        // alert('Вы добавлены в список админов!')
+    });
+  };
+  const onFailure = (res) => {
+    console.log(res);
+  };
+  return (
+    <LoginWrapper>
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={"single_host_origin"}
+        style={{ marginTop: "100px" }}
+        isSignedIn={true}
+      />
+    </LoginWrapper>
+  );
+};
